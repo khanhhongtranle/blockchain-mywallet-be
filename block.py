cfe3e10b330d5e617d6bdfe4da8f0b0ab7f094a5
khanhhongtranle <khanhhong141049@gmail.com>
2021-04-15 14:05:01 +0700

    validate block, blockchain

--- block.py
+++ block.py
@@ -1,16 +1,29 @@
 import hashlib
 
 class Block:
-    def __init__(self, index, previousHash, timestamp, data):
+    def __init__(self, index: int, previousHash: str, timestamp: int, data: str):
+        self.hash: str
         self.index = index
         self.previousHash = previousHash
         self.timestamp = timestamp
         self.data = data
         self.hash = self.calculateHash()
 
-    def calculateHash(self):
+    def calculateHash(self,block=None):
         sha256 = hashlib.sha256()
-        sha256.update(str(self.index) + str(self.previousHash) + str(self.timestamp) + str(self.data))
-        return self.hash
+        if block == None:
+            sha256.update(str(self.index) + str(self.previousHash) + str(self.timestamp) + str(self.data))
+        else:
+            sha256.update(str(block.index) + str(block.previousHash) + str(block.timestamp) + str(block.data))
+        return sha256.digest()
 
+    def validateBlock(self, previousBlock):
+        if self.index - previousBlock.index != 1:
+            return False
+        if self.previousHash != previousBlock.hash:
+            return False
+        if self.calculateHash(block=self) != self.hash:
+            return False
 
+    def validateBlockStructure(self):
+        return type(self.index) is int and type(self.hash) is str and type(self.previousHash) is str and type(self.timestamp) is int and type(self.data) is str
--- blockchain.py
+++ blockchain.py
@@ -1,21 +1,34 @@
 from block import Block
 from time import time
 
+
 class BlockChain:
     def __init__(self):
         self.__chains = []
         self.__transactions = []
-        self.__chains.append(self.genesisBlock())
+        firstBlock = self.genesisBlock()
+        self.addBlockToChains(firstBlock)
         pass
 
+    def addBlockToChains(self, newBlock):
+        self.__chains.append(newBlock)
+
+    def getLatestBlock(self):
+        return self.__chains[len(self.__chains) - 1]
+
     def genesisBlock(self):
-        return Block(index=0, previousHash="", timestamp=time(), data="first block")
+        return Block(index=0, previousHash="", timestamp=int(time()), data="first block")
 
     def generateNextBlock(self, data):
         previousBlock = self.getLatestBlock()
         nextIndex = previousBlock.index + 1
         nextTimestamp = time()
+        newBlock = Block(index=nextIndex, previousHash=previousBlock.hash, timestamp=int(nextTimestamp), data=data)
+        self.addBlockToChains(newBlock)
 
+    def validateBlockChain(self):
+        for index in [1, len(self.__chains)-1]:
+            if not self.__chains[index].validateBlock(self.__chains[index - 1]):
+                return False
 
-    def getLatestBlock(self):
-        return self.__chains[len(self.__chains) - 1]
+        return True
